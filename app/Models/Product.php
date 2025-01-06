a1f74f4b38d699c51be1bb0368eee3427485e9c2
Marcelo Melo Jr <marcelo@plastikastudio.com.br>
2025-01-06 15:57:24 -0300

    feat: product model, migration, seed and factory

---
+++ app/Models/Product.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    /** @use HasFactory<\Database\Factories\ProductFactory> */
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'description',
+        'price',
+        'quantity',
+        'active',
+    ];
+
+    protected $casts = [
+        'active' => 'boolean',
+    ];
+}
