5bbdd6fc96d66b170e4541b06625bd6c3453793a
WilliamJSS <williamsena088@gmail.com>
2025-01-08 19:35:43 -0300

    feat: cria rotas de autenticacao para api

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\ProductController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -19,15 +20,25 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+// ROTAS DE AUTENTICACAO
+
+Route::prefix("auth")->controller(AuthController::class)->group(function () {
+    Route::post("register", "register")->name('api.auth.register');
+    Route::post("login", "login")->name('api.auth.login');
+    Route::post("logout", "logout")->middleware("auth:sanctum")->name('api.auth.logout');
+});
+
+// ROTAS DE PRODUTO
+
 Route::prefix("product")->controller(ProductController::class)->group(function () {
     // rotas abertas
-    Route::get('/', 'list')->name('product.list');
-    Route::get('/{id}', 'show')->name('product.show');
+    Route::get('/', 'list')->name('api.product.list');
+    Route::get('/{id}', 'show')->name('api.product.show');
 
     // rotas protegidas por autenticacao
-    // Route::middleware('auth:sanctum')->group(function () {
-    Route::post('/', 'store')->name('product.store');
-    Route::put('/{id}', 'update')->name('product.update');
-    Route::delete('/destroy/{id}', 'destroy')->name('product.destroy');
-    // });
+    Route::middleware('auth:sanctum')->group(function () {
+        Route::post('/', 'store')->name('api.product.store');
+        Route::put('/{id}', 'update')->name('api.product.update');
+        Route::delete('/destroy/{id}', 'destroy')->name('api.product.destroy');
+    });
 });
